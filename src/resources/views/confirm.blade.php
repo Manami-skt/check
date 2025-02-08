8f6e5a0116e0f8a26097a7ff57a14f01e95864dd
Manami-skt <ariake.brass@icloud.com>
2025-02-08 11:36:19 +0900

    リモートリポジトリの変更

---
+++ src/resources/views/confirm.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/confirm.css') }}">
+@endsection
+
+@section('content')
+<div class="confirm__content">
+    <div class="confirm__heading">
+        <h2>Confirm</h2>
+    </div>
+
+    <form action="" class="form" action="/contacts" method="post">
+        @csrf
+        <div class="confirm-table">
+            <table class="confirm-table__inner">
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">お名前</th>
+                    <td class="confirm-table__text">
+                        <input type="text" name="name" value="{{ contact['name'] }}" readonly>
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">性別</th>
+                    <td class="confirm-table__text">
+                        <input type="text" name="gender" value="{{ $content['gender'] }}" readonly>
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">メールアドレス</th>
+                    <td class="confirm-table__text">
+                        <input type="email" name="email" value="{{ $content['email'] }}" readonly>
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">電話番号</th>
+                    <td class="confirm-table__text">
+                        <input type="tel" name="tel" value="{{ $content['tel'] }}" readonly>
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">住所</th>
+                    <td class="confirm-table__text">
+                        <input type="text" name="" value="{{ $content[''] }}" readonly>
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">建物名</th>
+                    <td class="confirm-table__text">
+                        <input type="text" name="" value="{{ $content[''] }}" readonly>
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">お問い合わせの種類</th>
+                    <td class="confirm-table__text">
+                        <input type="text" name="" value="{{ $content[''] }}" readonly>
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">お問い合わせ内容</th>
+                    <td class="confirm-table__text">
+                        <input type="text" name="content" value="{{ $content['content'] }}" readonly>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">送信</button>
+        </div>
+    </form>
+    <a href="/" class="correction">修正</a>
+</div>
+@endsection
---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,109 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+<div class="contact-form__content">
+    <div class="contact-form__heading">
+        <h2>Contact</h2>
+    </div>
+    <form action="/contacts" class="form" method="post">
+        @csrf
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お名前</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="last_name" placeholder="例: 山田" value="{{ old('last_name') }}">
+                    <input type="text" name="first_name" placeholder="例: 太郎" value="{{ old('first_name) }}">
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">性別</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="radio" name="gender" value="男性">男性
+                    <input type="radio" name="gender" value="女性">女性
+                    <input type="radio" name="gender" value="その他">その他
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">メールアドレス</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="email" name="email" placeholder="例: test@example.com">
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">電話番号</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="tel" name="tel" placeholder="">
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">住所</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="address" placeholder="例: 東京都渋谷区千駄ヶ谷1-2-3">
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">建物名</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" placeholder="例: 千駄ヶ谷マンション101">
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お問い合わせの種類</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <select name="" id="" placeholder="選択してください"></select>
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お問い合わせ内容</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form group-content">
+                <div class="form__input--text">
+                    <textarea name="content" id=""></textarea>
+                </div>
+            </div>
+        </div>
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">確認画面</button>
+        </div>
+    </form>
+</div>
+@endsection
